318ef886355627b201b46e69770bda946ded0af2
bala-amypo <bala@amypo.in>
2025-12-17 04:27:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studctl.java
+++ src/main/java/com/example/demo/controller/Studctl.java
@@ -6,5 +6,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/student")
 public class Studctl{
     @@Autowiredprivate Studservice ser;
-    
+    @PoastMapping("/add")
+    public Studentity addStudentity(@RequestBody Studentity st){
+        return ser.saveData(st);
+    }
+    @GetMapping(/"getall")
+    public Collection<Studentity> getAllStudent(){
+        return ser.getAll();
+    }
+    @GetMapping("/get/{id}")
+    public Studentity getStudentByid(@PathVariable int id){
+        return ser.getByid(id);
+    }
+    @PutMapping("/update/{id}")
+    public Studentity updateStudent(
+        @PathVariable int id,
+        @RequestBody Studentity st{
+            return ser.update(id,sr);
+        }
+    )
+    }
+    }
+    }
 }
